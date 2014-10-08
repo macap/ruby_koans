f75e608eaa0b4eee9a8a64b3bae69a829237827d
Maciek Caputa <maciek.caputa@gmail.com>
2014-10-08 23:41:55 +0200

    constans, controlstatements, exceptions, iteration, keywordarguments, true_and_false and triangle done

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,9 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  type=0;
+
+
+  raise TriangleError, "It's not a triangle" if (a+b<=c || b+c<=a || a+c<=b)
+
+
+  type=1 if a==b
+  type=type+1 if b==c
+  type=type+1 if a==c
+
+  return :equilateral if type==3
+  return :isosceles if type==1
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
 end
+
