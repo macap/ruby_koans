ce3093ae59fa0025e39529fde20db585d1be4507
Maciek Caputa <maciek.caputa@gmail.com>
2014-10-09 23:14:18 +0200

    dice project  done

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,19 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  def initialize
+    @values= []
+  end
+  def roll(number)
+    @values = []
+    (number).times do
+      @values << rand(1...6)
+    end
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
